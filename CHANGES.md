aedb9f168152b344583b7ded84adde222f03e8c5
AJ Dunn <andrew.dunn2@health.sa.gov.au>
2015-05-12 12:34:43 +0930

    version.php update to v1.0.2

--- CHANGES.md
+++ CHANGES.md
@@ -1,7 +1,11 @@
 # Changelog
 
-## v1.0.1
+## v1.0.2 (2015051200)
+- Fixed issue with modules not working if they are integers.
+- Fixed grades not updating Course Totals in User Reports.
+
+## v1.0.1 (2015050500)
 - Added fixes for users with Internet Explorer 8.
 
-## v.1.0.0
+## v.1.0.0 (2015042900)
 - Initial version.
--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_dmelearn';
-$plugin->version = 2015050500;
-$plugin->release = 'v1.0.1';
+$plugin->version = 2015051200;
+$plugin->release = 'v1.0.2';
 $plugin->requires = 2013051400; // Requires Moodle 2.5 or newer.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
