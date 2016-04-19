a70f8134c88f529a4b6ac40bfad13a3cbb1b9f0b
AJ Dunn <Andrew.Dunn2@sa.gov.au>
2016-04-19 13:39:47 +0930

    Add warning to course type/version 3

--- content/plates/layouts/3/footer.php
+++ content/plates/layouts/3/footer.php
@@ -1,16 +1,8 @@
 <?php //Course Footer ?>
 <div class="footer">
-    <!-- TODO -->
 </div>
-<?php //IE 8 Support ?>
-<!--[if lt IE 9]>
-<script src="js/html5shiv/html5shiv.min.js"></script>
-<script src="js/respond/respond.min.js"></script>
-<![endif]-->
 <?php //Jquery-UI ?>
 <script src="<?=$constants['base_js']?>jquery-ui.min.js"></script>
 <script src="<?=$constants['base_js']?>jquery.ui.touch-punch.min.js"></script>
 <?php //Bootstrap 3 JS  ?>
 <script src="js/bootstrap.min.js"></script>
-<?php //Audio JS  ?>
-<script src="<?=$constants['base_url']?>plugins/audiojs/audiojs/audio.min.js"></script>
