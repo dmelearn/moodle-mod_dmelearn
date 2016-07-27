6f209013882a1b4afd8b730f46305467e11b4411
CJ Faulkner <Christopher.Faulkner@health.sa.gov.au>
2016-07-27 14:40:43 +0930

    Move the download certificate & reset course alert into the course content wrapper

--- content/plates/layouts/2.1/shell.php
+++ content/plates/layouts/2.1/shell.php
@@ -10,40 +10,6 @@
 </div>
 
 <div class="course__content clearfix">
-    <?php //If the user has completed the course the reset and certificate buttons will appear ?>
-    <?php if ($page_data['data']['cert_data']['has_certificate']): ?>
-        <div class="alert alert-info clearfix row-fluid reset_and_cert_buttons">
-            <p class="pull-left"><strong>Well Done!</strong> You have completed the assessment for this course.</p>
-            <a class="btn pull-right btn-success"
-               href="<?= $constants['base_url'] ?>api/cert/download/<?= $page_data['data']['cert_data']['course_path'] ?>/<?= $page_data['data']['cert_data']['user_id'] ?>/<?= $page_data['data']['cert_data']['assessment_id'] ?>">Download
-                Certificate (PDF)</a>
-            <button type="button" class="btn btn-danger pull-right" data-toggle="modal" data-target=".reset_modal">Reset
-                Course
-            </button>
-        </div>
-        <?php //Modal ?>
-        <div class="modal fade reset_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                        <h3 id="myModalLabel">Are you sure you want to reset this course?</h3>
-                    </div>
-                    <div class="modal-body">
-                        <p>All of your assessments for this course will be reset so you can re-complete it again from
-                            the beginning.</p>
-                        <p><strong>You will not be able access previous certificates for this course once it has been
-                                reset.</strong></p>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-                        <button class="btn btn-danger reset_button" href="reset_course.php">Reset</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php endif; ?>
-
     <?php if ($navigation): ?>
         <script>
             hideMenu();
@@ -60,9 +26,7 @@
             // Checks if the side menu needs to be open or closed
             function hideMenu() {
                 var viewportWidth = $(window).width();
-                console.log(viewportWidth);
                 var openWindow = localStorage.getItem('dmSidebarOpen');
-                console.log(localStorage);
                 // If the local storage is set to close then keep it closed
                 if (openWindow == 'false') {
                     console.log('here we are');
@@ -129,6 +93,39 @@
 
     <?php //page content ?>
     <div class="course__content__wrapper clearfix">
+        <?php //If the user has completed the course the reset and certificate buttons will appear ?>
+        <?php if ($page_data['data']['cert_data']['has_certificate']): ?>
+            <div class="alert alert-info clearfix row-fluid reset_and_cert_buttons">
+                <p class="pull-left"><strong>Well Done!</strong> You have completed the assessment for this course.</p>
+                <a class="btn pull-right btn-success"
+                   href="<?= $constants['base_url'] ?>api/cert/download/<?= $page_data['data']['cert_data']['course_path'] ?>/<?= $page_data['data']['cert_data']['user_id'] ?>/<?= $page_data['data']['cert_data']['assessment_id'] ?>">Download
+                    Certificate (PDF)</a>
+                <button type="button" class="btn btn-danger pull-right" data-toggle="modal" data-target=".reset_modal">Reset
+                    Course
+                </button>
+            </div>
+            <?php //Modal ?>
+            <div class="modal fade reset_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                            <h3 id="myModalLabel">Are you sure you want to reset this course?</h3>
+                        </div>
+                        <div class="modal-body">
+                            <p>All of your assessments for this course will be reset so you can re-complete it again from
+                                the beginning.</p>
+                            <p><strong>You will not be able access previous certificates for this course once it has been
+                                    reset.</strong></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                            <button class="btn btn-danger reset_button" href="reset_course.php">Reset</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
         <div id="maincontent" class="course-body clearfix">
             <?php if (isset($page_data['data']['assessment_summary'])): ?>
                 <?php $this->insert('layouts/2.1/assessment_summary'); ?>
