87925e5f666d57658447701650ce1c888fdfba2f
Sami Rahman <samrahman@users.noreply.github.com>
2016-08-30 10:44:55 +0930

    Undefined index notice on course_version

--- content/plates/partials/prev_next.php
+++ content/plates/partials/prev_next.php
@@ -1,6 +1,10 @@
 <?php
 //Plates Template
-$version = $course_data['configuration']['course_version'];
+$version = isset($course_data['configuration']['course_version']) ? $course_data['configuration']['course_version'] : null;
+// If course version has not been set it should be a version 1
+if ($version === null) {
+    $version = 1;
+}
 ?>
 <?php if ($version === null || $version === 1) : ?>
 <div class = "prev_next row-fluid">
