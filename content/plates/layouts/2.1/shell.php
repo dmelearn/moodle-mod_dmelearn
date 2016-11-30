bea42378bdd494e0e6f8d997b067bbda0dffaca3
CJ Faulkner <Christopher.Faulkner@health.sa.gov.au>
2016-11-30 14:08:18 +1030

    Fix footer alignment within 2.1 styled courses

--- content/plates/layouts/2.1/shell.php
+++ content/plates/layouts/2.1/shell.php
@@ -12,59 +12,69 @@
 <div class="course__content clearfix">
     <?php if ($navigation) : ?>
         <script>
+          hideMenu();
+          // Do side menu check
+          $(function() {
+            checkMenuIcon();
+          });
+
+          // Check again when the window is resized
+          $(window).resize(function() {
             hideMenu();
-            // Do side menu check
-            $(function() {
-                checkMenuIcon();
-            });
+          });
 
-            // Check again when the window is resized
-            $(window).resize(function() {
-                hideMenu();
-            });
+          // Resize the course content so the footer is below the sidebar always
+          function contentMinHeight() {
+            var sidebar = $('.course__sidebar');
+            var courseBody = $('.course__content__wrapper');
 
-            // Checks if the side menu needs to be open or closed
-            function hideMenu() {
-                var viewportWidth = $(window).width();
-                var openWindow = localStorage.getItem('dmSidebarOpen');
-                // If the local storage is set to close then keep it closed
-                if (openWindow == 'false') {
-                    $(".course__content").addClass("closed-menu");
-                    $('#nav-box ul').removeClass('open');
-                } else {
-                    if (viewportWidth < 990) {
-                        $(".course__content").addClass("closed-menu");
-                        $('#nav-box ul').removeClass('open');
-                        localStorage.setItem('dmSidebarOpen', false);
-                    } else {
-                        $(".course__content").removeClass("closed-menu");
-                        $('#nav-box ul').addClass('open');
-                        localStorage.setItem('dmSidebarOpen', true);
-                    }
-                }
+            if (sidebar.innerHeight() >= courseBody.innerHeight()) {
+              courseBody.css({minHeight: sidebar.innerHeight()});
             }
+          }
 
-            // Check which icon should be shown on the menu
-            function checkMenuIcon() {
-                if (localStorage.getItem('dmSidebarOpen') == 'false' || $(window).width() < 990) {
-                    $('#nav-box ul').removeClass('open');
-                } else {
-                    $('#nav-box ul').addClass('open');
-                }
+          // Checks if the side menu needs to be open or closed
+          function hideMenu() {
+            var viewportWidth = $(window).width();
+            var openWindow = localStorage.getItem('dmSidebarOpen');
+            // If the local storage is set to close then keep it closed
+            if (openWindow == 'false') {
+              $(".course__content").addClass("closed-menu");
+              $('#nav-box ul').removeClass('open');
+            } else {
+              if (viewportWidth < 990) {
+                $(".course__content").addClass("closed-menu");
+                $('#nav-box ul').removeClass('open');
+                localStorage.setItem('dmSidebarOpen', false);
+              } else {
+                $(".course__content").removeClass("closed-menu");
+                $('#nav-box ul').addClass('open');
+                localStorage.setItem('dmSidebarOpen', true);
+              }
             }
+          }
 
-            // This function is called when the close menu button is clicked
-            function smallScreen() {
-
-                $('.course__content').toggleClass('closed-menu')
-                $('#nav-box ul').toggleClass('open');
-
-                if ($('.course__content').hasClass('closed-menu')) {
-                    localStorage.setItem('dmSidebarOpen', false);
-                } else {
-                    localStorage.setItem('dmSidebarOpen', true);
-                }
+          // Check which icon should be shown on the menu
+          function checkMenuIcon() {
+            if (localStorage.getItem('dmSidebarOpen') == 'false' || $(window).width() < 990) {
+              $('#nav-box ul').removeClass('open');
+            } else {
+              $('#nav-box ul').addClass('open');
             }
+          }
+
+          // This function is called when the close menu button is clicked
+          function smallScreen() {
+            $('.course__content').toggleClass('closed-menu')
+            $('#nav-box ul').toggleClass('open');
+
+            if ($('.course__content').hasClass('closed-menu')) {
+              localStorage.setItem('dmSidebarOpen', false);
+            } else {
+              localStorage.setItem('dmSidebarOpen', true);
+            }
+          }
+
         </script>
         <div class="course__sidebar__bg"></div>
         <aside class="course__sidebar">
@@ -135,6 +145,16 @@
             </section>
         </div>
     </div>
+
+    <script>
+      // Adjust the height of the content before the content is loaded
+      contentMinHeight();
+      // Adjust
+      $(document).ready(function() {
+        contentMinHeight();
+      });
+    </script>
+
 </div>
 
 <?php if (isset($course_data['glossary']['glossary_items']) && $course_data['glossary']) : ?>
