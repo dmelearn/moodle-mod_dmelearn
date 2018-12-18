4852ec2fc4e7c6a640e17ce9bd0dc5ec3fb93f20
AJ Dunn <andrew.dunn2@outlook.com.au>
2018-12-18 14:21:15 +1030

    Fix ‘Unknown table specified in objecttable field’ message
    Shows up when viewing a DM course with Debugging on and set to DEVELOPER and ‘Display debug messages’ on.

--- classes/event/course_module_viewed.php
+++ classes/event/course_module_viewed.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 class course_module_viewed extends \core\event\course_module_viewed {
     protected function init() {
-        $this->data['objecttable'] = 'DMELEARN';
+        $this->data['objecttable'] = 'dmelearn';
         parent::init();
     }
     // You might need to override get_url() and get_legacy_log_data() if view mode needs to be stored as well.
