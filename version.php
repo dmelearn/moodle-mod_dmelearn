201a0958760e413b47508ed5611f6d1c37a672ca
AJ Dunn <andrew.dunn2@outlook.com.au>
2019-11-21 17:04:11 +1030

    Update to Version v1.9.0 (2019112100)

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_dmelearn';
-$plugin->version = 2018121901;
-$plugin->release = 'v1.8.2';
+$plugin->version = 2019112100;
+$plugin->release = 'v1.9.0';
 $plugin->requires = 2013111800; // Requires Moodle 2.6 or newer.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
